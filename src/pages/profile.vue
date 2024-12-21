aab9c0e77fe6604ef579c5b4e172eefa1b1ce696
Mateus Gomes <mateusgomes.dev@gmail.com>
2024-12-21 08:03:32 -0300

    refactor: isolate web repository

---
+++ src/pages/profile.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-card>
+    <v-row>
+      <v-col cols="12" sm="12" md="6" class="mt-4">
+        <dashboard-finance-card title="Renda Mensal">
+          <dashboard-income-table />
+        </dashboard-finance-card>
+      </v-col>
+      <v-col cols="12" sm="12" md="6" class="mt-4">
+        <dashboard-finance-card title="Despesas Essenciais" height="400">
+          <dashboard-expense-table :expenses="essentialExpensesMock" />
+        </dashboard-finance-card>
+      </v-col>
+    </v-row>
+  </v-card>
+</template>
